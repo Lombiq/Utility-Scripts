c2e5283e21b200617c83e150539b24a6bfb6089a
Albert Hajdu <albert.hajdu@lombiq.com>
2015-11-21 10:52:23 +0100

    Styling path adder
    --HG--
    branch : issue/INFRA-18

--- AddCurrentPathToPSModulePath.ps1
+++ AddCurrentPathToPSModulePath.ps1
@@ -1,10 +1,13 @@
 ﻿<#
 .Synopsis
    Adds the current path to the PSModulePath environment variable
+
 .DESCRIPTION
    Only adds the current path if it doesn't exist yet. Also adds it to the $PSModulePath variable so the modules will available in the current console too.
+
 .EXAMPLE
    .\AddCurrentPathToPSModulePath.ps1
+
 #>
 $newModulePath = "$PSScriptRoot\"
 if($env:PSModulePath -split ';' -notcontains $newModulePath)
