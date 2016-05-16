cc186942d27310347590150b4edda80491a2856f
Benedek Farkas <benedek.farkas@lombiq.com>
2016-05-16 23:18:26 +0200

    Import-BacpacToSqlServer: if there's a server named "MSSQLSERVER" then choosing it as the preferred one
    --HG--
    branch : dev

--- Import-BacpacToSqlServer/Import-BacpacToSqlServer.psm1
+++ Import-BacpacToSqlServer/Import-BacpacToSqlServer.psm1
@@ -95,14 +95,22 @@ function Import-BacpacToSqlServer
                 $serverServices = (Get-WmiObject win32_Service -Computer $env:COMPUTERNAME | Where-Object { $PSItem.Name -match "MSSQL" -and $PSItem.PathName -match "sqlservr.exe" })
                 $servicePath = ""
 
+                # No SQL Servers installed.
                 if ($serverServices -eq $null)
                 {
                     throw ("Could not find any SQL Server services!")
                 }
+                # Only one SQL Server installed.
                 elseif ($serverServices.Count -eq $null)
                 {
                     $servicePath = $serverServices.PathName
                 }
+                # More than one SQL Servers installed and one of them is named "MSSQLSERVER", so let's choose that.
+                elseif (($serverServices | Where-Object { $PSItem.Name -eq "MSSQLSERVER" }) -ne $null)
+                {
+                    $servicePath = ($serverServices | Where-Object { $PSItem.Name -eq "MSSQLSERVER" }).PathName
+                }
+                # More than one SQL Servers installed, choosing the first one.
                 else
                 {
                     $servicePath = $serverServices[0].PathName
