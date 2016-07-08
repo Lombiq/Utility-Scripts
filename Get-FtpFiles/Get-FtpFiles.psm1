c0a1ae03b718fa40f28f5096ac022a8d22af9471
Benedek Farkas <benedek.farkas@lombiq.com>
2016-07-08 20:24:50 +0200

    Adding Get-FtpFiles PS module (depends on WinSCP)
    --HG--
    branch : dev

---
+++ Get-FtpFiles/Get-FtpFiles.psm1
@@ -0,0 +1,94 @@
+﻿# Session.FileTransferProgress event handler.
+function FileTransferProgress
+{
+    param
+    (
+        [WinSCP.FileTransferProgressEventArgs] $event
+    )
+
+    if ($script:lastFileName -ne $null -and $script:lastFileName -ne $event.FileName)
+    {
+        Write-Host
+    }
+
+    $currentFileName = $event.FileName
+    $currentFileProgress = $event.FileProgress
+
+    # If the progress changed compared to the previous state.
+    if ($currentFileName -ne $script:lastFileName -or $currentFileProgress -ne $script:lastFileProgress)
+    {
+        # Print transfer progress.
+        Write-Host ("$($event.FileName): $($event.FileProgress * 100)%, Overall: $($event.OverallProgress * 100)%")
+ 
+        # Remember the name of the last file reported.
+        $script:lastFileName = $event.FileName
+        $script:lastFileProgress = $event.FileProgress        
+    }
+}
+
+
+function Get-FtpFiles
+{
+    [CmdletBinding()]
+    [Alias("gff")]
+    Param
+    (
+        # The path of a folder that contains "WinSCPnet.dll" and "WinSCPnet.exe".
+        [Parameter(Mandatory=$true)]
+        [string] $WinSCPPath = $(throw "You need to provide the path to a folder that contains `"WinSCPnet.dll`" and `"WinSCPnet.exe`"."),
+
+        [Parameter(Mandatory=$true)]
+        [string] $FtpHostName,
+
+        [Parameter(Mandatory=$true)]
+        [string] $FtpUsername,
+
+        [Parameter(Mandatory=$true)]
+        [string] $FtpPassword,
+
+        [Parameter(Mandatory=$true)]
+        [string] $DownloadSourcePath,
+
+        [Parameter(Mandatory=$true)]
+        [string] $DownloadDestinationPath
+    )
+
+    Begin
+    {
+        [Reflection.Assembly]::LoadFrom("\\$WinSCPPath\WinSCPnet.dll") | Out-Null
+
+        $script:lastFileName = ""
+        $script:lastFileProgress = ""
+    }
+    Process
+    {
+        $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+            Protocol = [WinSCP.Protocol]::Ftp
+            HostName = $FtpHostName
+            UserName = $FtpUsername
+            Password = $FtpPassword
+        }
+
+        $session = New-Object WinSCP.Session
+
+        try
+        {
+            $session.add_FileTransferProgress({ FileTransferProgress($_) })
+
+            $session.Open($sessionOptions)
+         
+            if ($session.FileExists($DownloadSourcePath))
+            {
+                $session.GetFiles("$DownloadSourcePath/*", "$DownloadDestinationPath\*").Check()
+            }
+            else
+            {
+                throw ("The path `"$DownloadSourcePath`" is invalid!")
+            }
+        }
+        finally
+        {
+            $session.Dispose()
+        }
+    }
+}
