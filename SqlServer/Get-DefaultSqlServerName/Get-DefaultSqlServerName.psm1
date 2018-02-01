15791ccb8256853ff80cfd7afb773f7f378f1b34
Benedek Farkas <benedek.farkas@lombiq.com>
2018-02-01 21:56:29 +0100

    Factoring out Get-DefaultSqlServerName from Import-BacpacToSqlServer
    --HG--
    branch : dev

---
+++ SqlServer/Get-DefaultSqlServerName/Get-DefaultSqlServerName.psm1
@@ -0,0 +1,38 @@
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
+
+function Get-DefaultSqlServerName
+{
+    [CmdletBinding()]
+    [Alias("gdssm")]
+    [OutputType([string])]
+    Param()
+
+    Process
+    {
+        $serverServices = (Get-WmiObject win32_Service -Computer $env:COMPUTERNAME | Where-Object { $PSItem.Name -match "MSSQL" -and $PSItem.PathName -match "sqlservr.exe" })
+        $servicePath = ""
+
+        # No SQL Servers installed.
+        if ($serverServices -eq $null)
+        {
+            throw ("Could not find any SQL Server services!")
+        }
+        # Only one SQL Server installed.
+        elseif ($serverServices.Count -eq $null)
+        {
+            $servicePath = $serverServices.PathName
+        }
+        # More than one SQL Servers installed and one of them is named "MSSQLSERVER" (default name), so let's choose that.
+        elseif (($serverServices | Where-Object { $PSItem.Name -eq "MSSQLSERVER" }) -ne $null)
+        {
+            $servicePath = ($serverServices | Where-Object { $PSItem.Name -eq "MSSQLSERVER" }).PathName
+        }
+        # More than one SQL Servers installed, choosing the first one.
+        else
+        {
+            $servicePath = $serverServices[0].PathName
+        }
+
+        return $servicePath.Substring($servicePath.LastIndexOf(" -s") + 3)
+    }
+}
--- SqlServer/Import-BacpacToSqlServer/Import-BacpacToSqlServer.psm1
+++ SqlServer/Import-BacpacToSqlServer/Import-BacpacToSqlServer.psm1
@@ -122,31 +122,7 @@ function Import-BacpacToSqlServer
         # SqlServerName is not defined, so let's try to find one.
         if ([string]::IsNullOrEmpty($SqlServerName))
         {
-            $serverServices = (Get-WmiObject win32_Service -Computer $env:COMPUTERNAME | Where-Object { $PSItem.Name -match "MSSQL" -and $PSItem.PathName -match "sqlservr.exe" })
-            $servicePath = ""
-
-            # No SQL Servers installed.
-            if ($serverServices -eq $null)
-            {
-                throw ("Could not find any SQL Server services!")
-            }
-            # Only one SQL Server installed.
-            elseif ($serverServices.Count -eq $null)
-            {
-                $servicePath = $serverServices.PathName
-            }
-            # More than one SQL Servers installed and one of them is named "MSSQLSERVER" (default name), so let's choose that.
-            elseif (($serverServices | Where-Object { $PSItem.Name -eq "MSSQLSERVER" }) -ne $null)
-            {
-                $servicePath = ($serverServices | Where-Object { $PSItem.Name -eq "MSSQLSERVER" }).PathName
-            }
-            # More than one SQL Servers installed, choosing the first one.
-            else
-            {
-                $servicePath = $serverServices[0].PathName
-            }
-
-            $DataSource = ".\" + $servicePath.Substring($servicePath.LastIndexOf(" -s") + 3)            
+            $DataSource = ".\$(Get-DefaultSqlServerName)"
         }
         else
         {
