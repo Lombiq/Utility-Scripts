48f159ffbd61463a5f79ae22145ee3e5fc9c4f48
Benedek Farkas <benedek.farkas@lombiq.com>
2018-02-01 21:12:25 +0100

    Making Import-BacpacToSqlServer a lot smarter regarding parameter handling
    --HG--
    branch : dev

--- Import-BacpacToSqlServer/Import-BacpacToSqlServer.psm1
+++ Import-BacpacToSqlServer/Import-BacpacToSqlServer.psm1
@@ -21,29 +21,37 @@
 
 function Import-BacpacToSqlServer
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = "ByServerAndDatabaseName")]
     [Alias("ipbpss")]
     [OutputType([bool])]
     Param
     (
         [Parameter(HelpMessage = "The path to the `"SqlPackage`" executable that performs the import process. When not defined, it will try to find the executable installed with the latest SQL Server.")]
-        [string] $SqlPackageExecutablePath,
+        [Parameter(ParameterSetName = "ByConnectionString")]
+        [Parameter(ParameterSetName = "ByServerAndDatabaseName")]
+        [string] $SqlPackageExecutablePath = "",
 
         [Parameter(Mandatory = $true, HelpMessage = "The path to the .bacpac file to import.")]
+        [Parameter(ParameterSetName = "ByConnectionString")]
+        [Parameter(ParameterSetName = "ByServerAndDatabaseName")]
         [string] $BacpacPath = $(throw "You need to specify the path to the .bacpac file to import."),
 
-        [Parameter(HelpMessage = "The connection string that defines the server and database to import the .bacpfile to. If not defined, it will be build using the `"SqlServerName`" and `"DatabaseName`" variables.")]
-        [string] $ConnectionString,
+        [Parameter(HelpMessage = "The connection string that defines the server and database to import the .bacpfile to. If not defined, it will be built using the `"SqlServerName`" and `"DatabaseName`" variables.")]
+        [Parameter(Mandatory = $true, ParameterSetName = "ByConnectionString")]
+        [string] $ConnectionString = "",
 
         [Parameter(HelpMessage = "The name of the SQL Server instance that will host the imported database. If not defined, it will be determined from the system registry.")]
-        [string] $SqlServerName,
+        [Parameter(ParameterSetName = "ByServerAndDatabaseName")]
+        [string] $SqlServerName = "",
 
         [Parameter(HelpMessage = "The name of the database that will be created for the imported .bacpac file. If not defined, it will be the name of the imported file.")]
-        [string] $DatabaseName
+        [Parameter(ParameterSetName = "ByServerAndDatabaseName")]
+        [string] $DatabaseName = ""
     )
 
     Process
     {
+        # Setting up SQL Package executable path.
         $sqlPackageExecutablePath = ""
         if (![string]::IsNullOrEmpty($SqlPackageExecutablePath) -and (Test-Path $SqlPackageExecutablePath))
         {
@@ -77,89 +85,114 @@ function Import-BacpacToSqlServer
 
 
 
+        # Checking the validity of the bacpac file.
         $bacpacFile = Get-Item $BacpacPath
-
         if ($bacpacFile -eq $null -or !($bacpacFile -is [System.IO.FileInfo]) -or !($bacpacFile.Extension -eq ".bacpac"))
         {
             throw ("The .bacpac file is not found at `"$BacpacPath`"!")
         }
 
-        [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
 
-        if ([string]::IsNullOrEmpty($ConnectionString))
+
+        # Given that the parameter set uses the ConnectionString, we're deconstructing it to check its validity.
+        if ($PSCmdlet.ParameterSetName -eq "ByConnectionString")
         {
-            $DataSource = ""
-            
-            if ([string]::IsNullOrEmpty($SqlServerName))
+            $connectionStringSegments = $ConnectionString.Split(';', [System.StringSplitOptions]::RemoveEmptyEntries)
+
+            $serverSegment = $connectionStringSegments | Where-Object { $PSItem.StartsWith("Data Source=") }
+            if (!([string]::IsNullOrEmpty($serverSegment)))
             {
-                $serverServices = (Get-WmiObject win32_Service -Computer $env:COMPUTERNAME | Where-Object { $PSItem.Name -match "MSSQL" -and $PSItem.PathName -match "sqlservr.exe" })
-                $servicePath = ""
-
-                # No SQL Servers installed.
-                if ($serverServices -eq $null)
-                {
-                    throw ("Could not find any SQL Server services!")
-                }
-                # Only one SQL Server installed.
-                elseif ($serverServices.Count -eq $null)
-                {
-                    $servicePath = $serverServices.PathName
-                }
-                # More than one SQL Servers installed and one of them is named "MSSQLSERVER", so let's choose that.
-                elseif (($serverServices | Where-Object { $PSItem.Name -eq "MSSQLSERVER" }) -ne $null)
-                {
-                    $servicePath = ($serverServices | Where-Object { $PSItem.Name -eq "MSSQLSERVER" }).PathName
-                }
-                # More than one SQL Servers installed, choosing the first one.
-                else
-                {
-                    $servicePath = $serverServices[0].PathName
-                }
-
-                $DataSource = ".\" + $servicePath.Substring($servicePath.LastIndexOf(" -s") + 3)
-
-                if (!(ConfirmSqlServer($DataSource)))
-                {
-                    $DataSource = "localhost"
-
-                    if (!(ConfirmSqlServer($DataSource)))
-                    {
-                        throw ("Could not find any SQL Server instances!")
-                    }
-                }
-            }
-            else
-            {
-                $DataSource = ".\$SqlServerName"
-
-                if (!(ConfirmSqlServer($DataSource)))
-                {
-                    throw ("The specified name of the SQL Server is invalid!")
-                }
+                $SqlServerName = $serverSegment.Split('=', [System.StringSplitOptions]::RemoveEmptyEntries)[1]
             }
 
-            if ([string]::IsNullOrEmpty($DatabaseName))
+            $databaseSegment = $connectionStringSegments | Where-Object { $PSItem.StartsWith("Initial Catalog=") }
+            if (!([string]::IsNullOrEmpty($databaseSegment)))
             {
-                $DatabaseName = $bacpacFile.BaseName
+                $DatabaseName = $databaseSegment.Split('=', [System.StringSplitOptions]::RemoveEmptyEntries)[1]
             }
-
-            $server = New-Object ("Microsoft.SqlServer.Management.Smo.Server") $DataSource
-
-            $databaseExists = ($server.Databases | Where-Object { $PSItem.Name -eq $DatabaseName }) -ne $null
-
-            if ($databaseExists)
-            {
-                $originalDatabaseName = $DatabaseName
-                $DatabaseName += "-" + [System.Guid]::NewGuid()
-            }
-
-            $ConnectionString = "Data Source=$DataSource;Initial Catalog=$DatabaseName;Integrated Security=True;"
         }
 
+
+        
+        # Checking the validity of the SqlServerName variable.
+        [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
+        $DataSource = ""
+        $invalidSqlServer = $false
+
+        # SqlServerName is not defined, so let's try to find one.
+        if ([string]::IsNullOrEmpty($SqlServerName))
+        {
+            $serverServices = (Get-WmiObject win32_Service -Computer $env:COMPUTERNAME | Where-Object { $PSItem.Name -match "MSSQL" -and $PSItem.PathName -match "sqlservr.exe" })
+            $servicePath = ""
+
+            # No SQL Servers installed.
+            if ($serverServices -eq $null)
+            {
+                throw ("Could not find any SQL Server services!")
+            }
+            # Only one SQL Server installed.
+            elseif ($serverServices.Count -eq $null)
+            {
+                $servicePath = $serverServices.PathName
+            }
+            # More than one SQL Servers installed and one of them is named "MSSQLSERVER" (default name), so let's choose that.
+            elseif (($serverServices | Where-Object { $PSItem.Name -eq "MSSQLSERVER" }) -ne $null)
+            {
+                $servicePath = ($serverServices | Where-Object { $PSItem.Name -eq "MSSQLSERVER" }).PathName
+            }
+            # More than one SQL Servers installed, choosing the first one.
+            else
+            {
+                $servicePath = $serverServices[0].PathName
+            }
+
+            $DataSource = ".\" + $servicePath.Substring($servicePath.LastIndexOf(" -s") + 3)            
+        }
+        else
+        {
+            $DataSource = ".\$SqlServerName"
+        }
+
+        if (!(ConfirmSqlServer($DataSource)))
+        {
+            Write-Warning ("Could not find SQL Server at `"$DataSource`"!")
+            $DataSource = "localhost"
+
+            if (!(ConfirmSqlServer($DataSource)))
+            {
+                throw ("Could not find any SQL Server instances!")
+            }
+        }
+
+
+
+        # Checking DatabaseName validity and handling if there's already a database with this name on the target server.
+        if ([string]::IsNullOrEmpty($DatabaseName))
+        {
+            $DatabaseName = $bacpacFile.BaseName
+        }
+
+        $server = New-Object ("Microsoft.SqlServer.Management.Smo.Server") $DataSource
+        $databaseExists = ($server.Databases | Where-Object { $PSItem.Name -eq $DatabaseName }) -ne $null
+
+        if ($databaseExists)
+        {
+            $originalDatabaseName = $DatabaseName
+            $DatabaseName += "-" + [System.Guid]::NewGuid()
+        }
+
+
+
+        # And now we get to actually importing the database after constructing a connection string with validated details.
+        $ConnectionString = "Data Source=$DataSource;Initial Catalog=$DatabaseName;Integrated Security=True;"
         & "$sqlPackageExecutablePath" /Action:Import /SourceFile:"$BacpacPath" /TargetConnectionString:"$ConnectionString"
 
+
+
+        # Importing the database was successful.
         if ($LASTEXITCODE -eq 0)
         {
+            # If there was a database with an idendical name on the target server, we'll swap it with the one we've just imported.
             if ($databaseExists)
             {
                 $server = New-Object ("Microsoft.SqlServer.Management.Smo.Server") $DataSource
@@ -170,6 +203,7 @@ function Import-BacpacToSqlServer
                 Write-Warning ("The original database with the name `"$originalDatabaseName`" has been deleted and the imported one has been renamed to use that name.")
             }
         }
+        # Importing the database failed.
         else
         {
             if ($databaseExists)
