aca45d8cecb876553c30a467fb90434920ca3596
Benedek Farkas <benedek.farkas@lombiq.com>
2022-04-07 11:58:05 +0200

    Adding PS module Rename-ChildItemsToAsciiRecursively to rename all folders and files under the specified path to have only ASCII characters in their names

---
+++ Utilities/Rename-ChildItemsToAsciiRecursively/Rename-ChildItemsToAsciiRecursively.psm1
@@ -0,0 +1,27 @@
+function Rename-ChildItemsToAsciiRecursively
+{
+    Param
+    (
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
+        [string] $Path
+    )
+
+    Process
+    {
+        foreach ($item in Get-ChildItem $Path)
+        {
+            if ($item.PSIsContainer)
+            {
+                Rename-ChildItemsToAsciiRecursively -Path $item.FullName
+            }
+
+            $asciiName = [System.Text.Encoding]::ASCII.GetString([System.Text.Encoding]::ASCII.GetBytes($item.Name)).Replace('?', '_')
+
+            if ($item.Name -ne $asciiName)
+            {
+                Write-Host ("Renaming `"$($item.FullName)`" to `"$asciiName`".")
+                $item | Rename-Item -NewName $asciiName
+            }
+        }
+    }
+}
