3c0ac6cef0e99d44a17be0ffda51ad0894903834
Benedek Farkas <benedek.farkas@lombiq.com>
2022-05-03 17:35:00 +0200

    Simplifying Update-DotNetDevelopmentCertificateHttps

--- DotNetCLI/Update-DotNetDevelopmentCertificateHttps/Update-DotNetDevelopmentCertificateHttps.psm1
+++ DotNetCLI/Update-DotNetDevelopmentCertificateHttps/Update-DotNetDevelopmentCertificateHttps.psm1
@@ -16,15 +16,8 @@ function Update-DotNetDevelopmentCertificateHttps
         $pfxPath = "$(Get-Location)\dotnet-dev-cert-https.pfx"
         dotnet dev-certs https --export-path $pfxPath
         
-        # Import the certificate into the user's trusted certificate store. Not using "dotnet dev-certs https --trust"
-        # here so that there's no confirmation popup, allowing to process to run in non-interactive mode.
-        Add-Type -AssemblyName System.Security
-        $certificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-        $certificate.Import($pfxPath)
-        $certificateStore = New-Object System.Security.Cryptography.X509Certificates.X509Store -ArgumentList "MY", CurrentUser
-        $certificateStore.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
-        $certificateStore.Add($certificate)
-        $certificateStore.Close()
+        # Import the certificate to the machine-wide certificate store.
+        Import-Certificate -FilePath $pfxPath -CertStoreLocation Cert:\CurrentUser\My
 
         # Clean up.
         Remove-Item $pfxPath -Force
