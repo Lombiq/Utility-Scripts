443b8094d0e56dac22ffc94c1295aca137f3175a
Benedek Farkas <benedek.farkas@lombiq.com>
2022-10-21 16:44:54 +0200

    Using full operator names instead of shorthands in ReloadEveryModuleInPSModulePaths.ps1

--- ReloadEveryModuleInPSModulePaths.ps1
+++ ReloadEveryModuleInPSModulePaths.ps1
@@ -8,8 +8,8 @@ if ($Verbose) { Reload-Module -Verbose } else { Reload-Module }
 
 # Reload everything.
 [Environment]::GetEnvironmentVariable("PSModulePath", "Machine") -split ';' |
-    ? { Test-Path -PathType Container $_ } |
+    Where-Object { Test-Path -PathType Container $_ } |
     # Don't try to reload the built-in modules. That's usually meaningless and if they do change you should reboot.
-    ? { $_ -notlike 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules*' } |
-    ? { $_ -notlike 'C:\Program Files\WindowsPowerShell\Modules*' } |
-    % { if ($Verbose) { Reload-Module $_ -Verbose } else { Reload-Module $_ } }
+    Where-Object { $_ -notlike 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules*' } |
+    Where-Object { $_ -notlike 'C:\Program Files\WindowsPowerShell\Modules*' } |
+    ForEach-Object { if ($Verbose) { Reload-Module $_ -Verbose } else { Reload-Module $_ } }
