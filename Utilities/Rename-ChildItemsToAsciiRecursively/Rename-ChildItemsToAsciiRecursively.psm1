ff3acbb574f8a9ab5e270849ab6a6ba7e84acb31
Benedek Farkas <benedek.farkas@lombiq.com>
2023-10-17 19:53:38 +0200

    Fixing Rename-ChildItemsToAsciiRecursively when renaming a folder if the target name already exists

--- Utilities/Rename-ChildItemsToAsciiRecursively/Rename-ChildItemsToAsciiRecursively.psm1
+++ Utilities/Rename-ChildItemsToAsciiRecursively/Rename-ChildItemsToAsciiRecursively.psm1
@@ -8,7 +8,30 @@
 
     Process
     {
-        Get-ChildItem $Path -Recurse | ForEach-Object {
+        Get-ChildItem $Path -Directory -Recurse | ForEach-Object {
+            $asciiName = [System.Text.Encoding]::ASCII.GetString([System.Text.Encoding]::ASCII.GetBytes($PSItem.Name)).Replace('?', '_')
+
+            if ($PSItem.Name -eq $asciiName) { return }
+
+            # If there's already a folder with the new name of the current one.
+            if (Test-Path (Join-Path $PSItem.Parent.FullName $asciiName))
+            {
+                # Move the contents of the current folder to the other one with the new name.
+                Get-ChildItem $PSItem.FullName -Recurse | Move-Item -Destination (Join-Path $PSItem.Parent.FullName $asciiName)
+
+                # Then delete the current folder.
+                Remove-Item $PSItem.FullName
+            }
+            else
+            {
+                Write-Verbose "Renaming `"$($_.FullName)`" to `"$asciiName`"."
+                $PSItem | Rename-Item -NewName $asciiName
+
+                New-Object PSObject -Property @{ Original = $PSItem.FullName; Renamed = $asciiName }
+            }
+        }
+
+        Get-ChildItem $Path -File -Recurse | ForEach-Object {
             $asciiName = [System.Text.Encoding]::ASCII.GetString([System.Text.Encoding]::ASCII.GetBytes($PSItem.Name)).Replace('?', '_')
 
             if ($PSItem.Name -eq $asciiName) { return }
