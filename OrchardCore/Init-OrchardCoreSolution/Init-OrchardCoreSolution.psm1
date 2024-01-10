1538d9eda5c4b8e9c4bc34e65eb3cb6c8732d712
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-01-10 16:55:42 +0100

    Adding a Init-OrchardCoreSolution proxy script that runs Initialize-OrchardCoreSolution for backwards compatibility

---
+++ OrchardCore/Init-OrchardCoreSolution/Init-OrchardCoreSolution.psm1
@@ -0,0 +1,29 @@
+﻿<#
+.Synopsis
+    Initializes an Orchard Core solution for a git repository. Deprecated, use Initialize-OrchardCoreSolution instead.
+#>
+
+function Init-OrchardCoreSolution
+{
+    [CmdletBinding()]
+    Param
+    (
+        [string] $Path = (Get-Location).Path,
+
+        [Parameter(Mandatory = $true)]
+        [string] $Name,
+
+        [string] $ModuleName,
+        [string] $ThemeName,
+        [string] $NuGetSource
+    )
+
+    Process
+    {
+        $warningMessage = 'Warning: "Init-OrchardCoreSolution" is the old and deprecated name of this module. ' +
+            'Use "Initialize-OrchardCoreSolution" instead.'
+        Write-Warning $warningMessage
+
+        Initialize-OrchardCoreSolution -Path $Path -Name $Name -ModuleName $ModuleName -ThemeName $ThemeName -NuGetSource $NuGetSource
+    }
+}
