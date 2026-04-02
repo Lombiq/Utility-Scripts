29af7a685bcff085f7225625d053cc02c2cb1ff1
Sára El-Saig <sara.el-saig@lombiq.com>
2026-04-02 15:30:47 +0200

    Ensure that the solution is created in SLNX format.

--- OrchardCore/Initialize-OrchardCoreSolution/Initialize-OrchardCoreSolution.psm1
+++ OrchardCore/Initialize-OrchardCoreSolution/Initialize-OrchardCoreSolution.psm1
@@ -44,7 +44,7 @@ function Initialize-OrchardCoreSolution
 
         dotnet new occms --output "$Path/src/$Name.Web"
 
-        dotnet new sln --output "$Path" --name "$Name"
+        dotnet new sln --output "$Path" --name "$Name" --format slnx
         dotnet sln "$Path/$Name.sln" add "$Path/src/$Name.Web/$Name.Web.csproj"
 
         if (-not [string]::IsNullOrEmpty($ModuleName))
