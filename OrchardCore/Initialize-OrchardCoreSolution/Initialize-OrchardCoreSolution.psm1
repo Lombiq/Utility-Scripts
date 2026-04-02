8ff6833f82f30189a1ae4b60a70fe504471ac21b
Sára El-Saig <sara.el-saig@lombiq.com>
2026-04-02 15:28:51 +0200

    Don't use shorthand format for dotnet new switches.

--- OrchardCore/Initialize-OrchardCoreSolution/Initialize-OrchardCoreSolution.psm1
+++ OrchardCore/Initialize-OrchardCoreSolution/Initialize-OrchardCoreSolution.psm1
@@ -42,21 +42,21 @@ function Initialize-OrchardCoreSolution
             dotnet new install OrchardCore.ProjectTemplates::2.2.0 --nuget-source $NuGetSource
         }
 
-        dotnet new occms -o "$Path/src/$Name.Web"
+        dotnet new occms --output "$Path/src/$Name.Web"
 
-        dotnet new sln -o "$Path" -n "$Name"
+        dotnet new sln --output "$Path" --name "$Name"
         dotnet sln "$Path/$Name.sln" add "$Path/src/$Name.Web/$Name.Web.csproj"
 
         if (-not [string]::IsNullOrEmpty($ModuleName))
         {
-            dotnet new ocmodulecms -n "$ModuleName" -o "$Path/src/Modules/$ModuleName"
+            dotnet new ocmodulecms --name "$ModuleName" --output "$Path/src/Modules/$ModuleName"
             dotnet add "$Path/src/$Name.Web/$Name.Web.csproj" reference "$Path/src/Modules/$ModuleName/$ModuleName.csproj"
             dotnet sln "$Path/$Name.sln" add "$Path/src/Modules/$ModuleName/$ModuleName.csproj"
         }
 
         if (-not [string]::IsNullOrEmpty($ThemeName))
         {
-            dotnet new octheme -n "$ThemeName" -o "$Path/src/Themes/$ThemeName"
+            dotnet new octheme --name "$ThemeName" --output "$Path/src/Themes/$ThemeName"
             dotnet add "$Path/src/$Name.Web/$Name.Web.csproj" reference "$Path/src/Themes/$ThemeName/$ThemeName.csproj"
             dotnet sln "$Path/$Name.sln" add "$Path/src/Themes/$ThemeName/$ThemeName.csproj"
         }
