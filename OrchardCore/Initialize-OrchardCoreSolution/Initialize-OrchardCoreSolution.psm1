4048fa4feeb3fd42ba46ffb3436ffc5ee7ac6565
Zoltan Lehoczky <zoltan.lehoczky@lombiq.com>
2026-07-14 16:00:52 +0200

    Updating templates to Orchard Core v3.0.1

--- OrchardCore/Initialize-OrchardCoreSolution/Initialize-OrchardCoreSolution.psm1
+++ OrchardCore/Initialize-OrchardCoreSolution/Initialize-OrchardCoreSolution.psm1
@@ -35,11 +35,11 @@ function Initialize-OrchardCoreSolution
 
         if ([string]::IsNullOrEmpty($NuGetSource))
         {
-            dotnet new install OrchardCore.ProjectTemplates@3.0.0
+            dotnet new install OrchardCore.ProjectTemplates@3.0.1
         }
         else
         {
-            dotnet new install OrchardCore.ProjectTemplates@3.0.0 --nuget-source $NuGetSource
+            dotnet new install OrchardCore.ProjectTemplates@3.0.1 --nuget-source $NuGetSource
         }
 
         dotnet new occms --output "$Path/src/$Name.Web"
